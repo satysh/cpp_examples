ca5f4ad19c0719ef61c216aa92ff3fa690a6966a
satyshev <satyshev@jinr.ru>
2018-05-17 17:47:00 +0300

    add 3 examples 17.05.18

---
+++ rndtmp.cpp
@@ -0,0 +1,19 @@
+#include <cstdio>
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "This system can generate up to " << TMP_MAX
+         << " Temporary names of up to " << L_tmpnam
+         << " caharacters.\n";
+
+    char pszName[L_tmpnam] = {'\0'};
+    cout << "Here are ten names:\n";
+    for (int i = 0; 10 > i; i++)
+    {
+        tmpnam(pszName);
+        cout << pszName << endl;
+    }
+    return 0;
+}
